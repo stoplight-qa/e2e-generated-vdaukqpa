84fed0af73ecc851370fef6fa3f5fc249d6f49dd
qa-vzzuz-0-gwka@example.com <qa-vzzuz-0-gwka@example.com>
2023-02-22 15:52:19 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15323.stoplight-dev.com/api/v1/projects/cHJqOjU4MQ/spectral.js?branch_slug=main&token=18311463-b9ac-4dea-999e-53fe0f1e61db";
+export default { extends: ruleset };
